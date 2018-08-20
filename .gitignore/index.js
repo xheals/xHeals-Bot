4cdc9edd86ce065a57f5277c05b36d35a3b985fc
xheals <42551832+xheals@users.noreply.github.com>
2018-08-20 22:35:30 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,40 @@
 const Discord = require("discord.js");
 
+var prefix ="x/";
+
 var bot = new Discord.Client()
 
 bot.on("ready", function() {
-    bot.user.setGame("xheals c'est mon chef");
+    bot.user.setGame("xheals mon chef");
     console.log("le bot à bien été connecté");
 });
 
-bot.login("NDgxMTIxNDczNzIxNzk0NTYw.Dlx6CA.b-7KQQR6HUoQAZoF4YJmvbJwOgU");
+bot.on "message", async function(message){
+    if (message.author.equal(bot.user))return;
+
+    if(x!message.content.startsWith(prefix))return;
+
+    var args = message.content.substring(PREFIX.length).split("");
+
+    switch(args[0].toLowerCase()) {
+        case"invite":
+        message.channel.send("", {
+            enbed: {
+                color: 0xFF0000,
+                author: message.author.name,
+                title: '',
+                fields: [{
+                    name: "Lien D'invitation discord"
+                    value: "https://discord.gg/aZQ6Yez"
+                    inline: false
+                }],
+                footer: {
+                    footer: "Partager ce lien à toous vos amis !",
+                },
+            }
+        });
+        break;
+    }
+}};
+
+bot.login(process.env.TOKEN);
