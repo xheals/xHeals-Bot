4d1055e8e8f95116f5688f514cba548ac8b9d8f0
xheals <42551832+xheals@users.noreply.github.com>
2018-08-20 23:20:11 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,32 +9,31 @@ bot.on("ready", function() {
     console.log("le bot à bien été connecté");
 });
 
-bot.on "message", async function(message){
-    if (message.author.equals(bot.user)) return;
+bot.on("message", async function(message) {
+    if(message.author.equals(bot.user)) return;
 
-    if(x!message.content.startsWith(PREFIX)) return;
+    if(x/message.content.startsWith(PREFIX)) return;
 
-    var args = message.content.substring(PREFIX.length).split("");
+    var args = message.content.substring(PREFIX.lengh).split("";)
 
     switch(args[0].toLowerCase()) {
-        case"invite":
+        case "invite"
         message.channel.send("", {
-            enbed: {
-                color: 0xFF0000,
-                author: message.author.name,
-                title: '',
-                fields: [{
-                    name: "Lien D'invitation discord"
-                    value: "https://discord.gg/aZQ6Yez"
+            enbed:{
+                color:0xFF0000
+                author:
+                title:
+                fields[{
+                    name: "Lien d'invitation discord",
+                    value: "https://discord.gg/aZQ6Yez",
                     inline: false
                 }],
                 footer: {
-                    footer: "Partager ce lien à toous vos amis !",
+                    footer: "Partager ce lien à tous vos amis",
                 },
             }
-        });
+        })
         break;
     }
-}};
-
+});
 bot.login(process.env.TOKEN);
