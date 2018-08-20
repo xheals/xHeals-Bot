ead9e8f52b5b5a7521698201b2c6c37275202fe1
xheals <42551832+xheals@users.noreply.github.com>
2018-08-20 22:57:05 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,18 +1,18 @@
 const Discord = require("discord.js");
 
-var prefix ="x/";
+var prefix = "x/";
 
-var bot = new Discord.Client()
+var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("xheals mon chef");
+    bot.user.setGame("x/help");
     console.log("le bot à bien été connecté");
 });
 
 bot.on "message", async function(message){
-    if (message.author.equal(bot.user))return;
+    if (message.author.equals(bot.user)) return;
 
-    if(x!message.content.startsWith(prefix))return;
+    if(x!message.content.startsWith(PREFIX)) return;
 
     var args = message.content.substring(PREFIX.length).split("");
 
